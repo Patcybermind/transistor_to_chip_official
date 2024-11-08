c5ecf4984a50a22b8f471d1e6d46361b6e40a175
patcybermind <pato.gonzalezcabrera@gmail.com>
2024-11-08 17:22:44 -0500

    latest

--- editor/src/app/page.js
+++ editor/src/app/page.js
@@ -4,12 +4,10 @@ import ThreeScene from '../components/ThreeScene';
 export default function Home() {
   return (
     <div>
-    <p>
-      
-      Hello World
-      
-    </p> 
+
     <ThreeScene />
+    
     </div>
   );
 }
+
